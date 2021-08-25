7fe6d53826e18be083c31bb8c91b2b01ca1d0faf
mahaomolise92 <mahaomolise92@gmail.com>
2021-08-25 11:19:43 +0200

    this is forked from bahmni appointments release-1.1 and added a code to pull patient art numbers

---
+++ api/src/main/java/org/openmrs/module/appointments/dao/AppointmentServiceDao.java
@@ -0,0 +1,22 @@
+package org.openmrs.module.appointments.dao;
+
+
+import org.openmrs.module.appointments.model.AppointmentService;
+import org.openmrs.module.appointments.model.AppointmentServiceType;
+import org.openmrs.module.appointments.model.AppointmentStatus;
+
+import java.util.Date;
+import java.util.List;
+
+public interface AppointmentServiceDao {
+
+    List<AppointmentService> getAllAppointmentServices(boolean includeVoided);
+
+    AppointmentService save(AppointmentService appointmentService);
+
+    AppointmentService getAppointmentServiceByUuid(String uuid);
+
+    AppointmentService getNonVoidedAppointmentServiceByName(String serviceName);
+
+    AppointmentServiceType getAppointmentServiceTypeByUuid(String uuid);
+}
